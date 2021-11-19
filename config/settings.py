447114ffa2b439401d1f1294f47188c4ef1b1c89
simonharel <simonolivierharel@gmail.com>
2021-11-19 12:35:51 -0500

    no message

--- config/settings.py
+++ config/settings.py
@@ -7,12 +7,14 @@ class BaseConfig():
 class DevConfig(BaseConfig):
     FLASK_ENV = 'development'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123onelove@localhost/peppa-pig-app-db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123onelove@localhost/peppa-pig-app-db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vkyjktygxovall:1d24d0247207fca1ef51ee59b322d57566498ba606cce3090001c414692381da@ec2-54-163-162-239.compute-1.amazonaws.com:5432/d2hn98c315q9bi'
 
 
 class ProductionConfig(BaseConfig):
     FLASK_ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123onelove@db-postgres:5432/flask-deploy'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123onelove@db-postgres:5432/flask-deploy'
+    SQLALCHEMY_DATABASE_URI = 'postgres://vkyjktygxovall:1d24d0247207fca1ef51ee59b322d57566498ba606cce3090001c414692381da@ec2-54-163-162-239.compute-1.amazonaws.com:5432/d2hn98c315q9bi'
 
 
 class TestConfig(BaseConfig):
