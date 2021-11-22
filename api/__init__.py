9022f41b8c3fe7d2ca65cc91fc7e153e9d689690
simonharel <simonolivierharel@gmail.com>
2021-11-22 13:48:49 -0500

    no message

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,8 @@ from flask_cors import CORS
 from api.apiUser import UserAPI
 from api.apiRoutes import api
 
+
 def build_api(app):
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
     api.init_app(app)
 
