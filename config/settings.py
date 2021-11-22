69b55bfd5d318cae7fafba30ef174a9405489276
simonharel <simonolivierharel@gmail.com>
2021-11-22 15:47:55 -0500

    no message

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ class ProductionConfig(BaseConfig):
     FLASK_ENV = 'production'
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:123onelove@db-postgres:5432/flask-deploy'
     SQLALCHEMY_DATABASE_URI = 'postgresql://vkyjktygxovall:1d24d0247207fca1ef51ee59b322d57566498ba606cce3090001c414692381da@ec2-54-163-162-239.compute-1.amazonaws.com:5432/d2hn98c315q9bi'
-    CORS_ENABLED = False
+    CORS_ENABLED = True
 
 class TestConfig(BaseConfig):
     FLASK_ENV = 'development'
