e826a823f5c291c0f950792444a1cb0a19044ef6
simonharel <simonolivierharel@gmail.com>
2021-11-22 15:52:11 -0500

    no message

--- api/__init__.py
+++ api/__init__.py
@@ -4,6 +4,7 @@ from api.apiRoutes import api
 
 
 def build_api(app):
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
     api.init_app(app)
 
