ddcba854154f68be58a70df417215a3d873a415b
simonharel <simonolivierharel@gmail.com>
2021-11-22 15:25:09 -0500

    no message

--- runServer.py
+++ runServer.py
@@ -22,6 +22,8 @@ def create_app():
     logger.info(f'Starting app in {config.APP_ENV} environment')
     app = Flask(__name__)
     app.config.from_object('config')
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
     build_api(app)
     init_db(app)
     CORS(app)
