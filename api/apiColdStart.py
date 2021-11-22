24407931e16b133cbdc1a763c4f6e8e92899334e
simonharel <simonolivierharel@gmail.com>
2021-11-22 15:55:49 -0500

    no message

--- api/apiColdStart.py
+++ api/apiColdStart.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, request
 from dbModels.sysRecColdStartModel import SysRecColdStartModel
-
+from flask_cors import cross_origin
 # Routes permettant d'obtenir les choix pour le cold start (liste des topics id)
 # et de sauvegarder le choix de l'utilisateur.
 # Ne retourne aucune recommandations !!!
@@ -14,6 +14,7 @@ class ColdStartAPI(Resource):
         choices = self._model.get_cold_start_choices()
         return {'choices': choices.tolist()}, 200
 
+    @cross_origin()
     def post(self):
         res = request.get_json()
         return self._model.save_cold_start_choice(cold_start_choice=res['result'], user_id=res['user_id']), 200
