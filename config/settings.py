8aec11da5ebd598cc127558844e9faa8af85178f
simonharel <simonolivierharel@gmail.com>
2022-01-12 12:48:57 -0500

    no message

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ class DevConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     FLASK_ENV = 'production'
-    SQLALCHEMY_DATABASE_URI = 'postgres://euupwctnkrfhjo:f002afeb5b737f82a21f4ebf10a17a8789d77c3094a907622bc05d25216d476b@ec2-18-209-169-66.compute-1.amazonaws.com:5432/d213rdfr3trsj7'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://euupwctnkrfhjo:f002afeb5b737f82a21f4ebf10a17a8789d77c3094a907622bc05d25216d476b@ec2-18-209-169-66.compute-1.amazonaws.com:5432/d213rdfr3trsj7'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
