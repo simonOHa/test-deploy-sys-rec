814771979aaae64bbe3a18bb008955bdac77ff88
simonharel <simonolivierharel@gmail.com>
2022-03-09 16:57:47 -0500

    no message

--- api/apiUser.py
+++ api/apiUser.py
@@ -10,7 +10,7 @@ CLIENT_SECRETS_FILE = "client_secret.json"
 
 class UserLoginAPI(Resource):
 
-    _model = UserModel()
+    #_model = UserModel()
 
     def __init__(self):
         pass
@@ -22,10 +22,10 @@ class UserLoginAPI(Resource):
             credentials = client.credentials_from_clientsecrets_and_code(CLIENT_SECRETS_FILE, ['email'], auth_code)
             http_auth = credentials.authorize(httplib2.Http()) # adds in the appropriate headers and then delegates to the original
 
-            user = self._model.query.filter_by(email=credentials.id_token['email']).first()
+            user = UserModel.query.filter_by(email=credentials.id_token['email']).first()
 
             if user is None:
-                is_admin, fic_acceptance, know_peppa_pig, is_test_1_completed,is_test_2_completed,is_test_3_completed = self._model.create_user(email=credentials.id_token['email'],
+                is_admin, fic_acceptance, know_peppa_pig, is_test_1_completed,is_test_2_completed,is_test_3_completed = UserModel().create_user(email=credentials.id_token['email'],
                                                                                    access_token=credentials.access_token,
                                                                                    refresh_token=credentials.refresh_token)
                 access_token = credentials.access_token
